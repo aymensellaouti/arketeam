fa7ec7d6a71c2338c13b6cdbe31a7ef2c37281d6
aymen2782 <aymen.sellaouti@gmail.com>
2021-02-23 09:21:30 +0100

    Init routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RotatingCardComponent } from './components/rotating-card/rotating-card.component';
+import { CvComponent } from './cv/cv/cv.component';
+import { TodoComponent } from './todo/todo/todo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RotatingCardComponent },
+  {path: 'todo', component: TodoComponent },
+  {path: 'cv', component: CvComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
